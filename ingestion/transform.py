8c1bca7af37f923cea284a762c47027a2c0126b2
ahmedkhan753 <ahmedk32410@gmail.com>
2026-01-29 17:31:31 +0500

    initializes transformation schema

---
+++ ingestion/transform.py
@@ -0,0 +1,15 @@
+class transform:
+    def __init__(self):
+        pass
+    def clean_data(self):
+        pass
+    def normalize_data(self):
+        pass
+    def symbol_to_logic(self):
+        pass
+    def keyword_mapping(self):
+        pass
+    def final_data_model(self):
+        pass
+    def validate_data(self):
+        pass
