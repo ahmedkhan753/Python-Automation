b5ac0d5ae61c1476790a60721490a23942857cc3
ahmedkhan753 <ahmedk32410@gmail.com>
2026-01-29 16:25:45 +0500

    added file extracter script

--- ingestion/extract.py
+++ ingestion/extract.py
@@ -1,7 +1,16 @@
+import os
+import pandas as pd
+
 class extract:
-    def __init__(self):
+    FOLDER = r'D:\PROJECTS\python-automation\pdf'
+    FILENAME ='Serie Ibiza-1-1-3.pdf'
+    FILE_PATH = os.path.join(FOLDER, FILENAME)
+
+    def __init__(self,FILE_PATH):
+        self.FILE_PATH = FILE_PATH
 
     def extract_tables(self):
+        df = pd.read_pdf(self.FILE_PATH)
 
     def extract_features(self):
 
